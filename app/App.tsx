24db9cee072e756dabd9a67d5e85b5e7bb53bbcd
Rafael Cerri <rafaelbcerri@gmail.com>
2019-03-27 00:16:42 -0300

    Use Typescript and add linter

--- app/App.tsx
+++ app/App.tsx
@@ -1,24 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+interface Props {}
+interface State {}
+
+export default class App extends Component<Props, State> {
+  public render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>A to React Native!</Text>
@@ -31,19 +25,19 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center'
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: 'center'
+  }
 });
